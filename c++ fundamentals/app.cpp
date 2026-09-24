42f0ed3624be642858a85fdba393a623ea9f84a1
salma waleed <salmawaleed20067@gmail.com>
2026-09-24 03:31:56 +0300

    C++ fundamentals course From  Lec 1 to Lec 13

---
+++ c++ fundamentals/app.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std ;
+
+int main()
+{ 
+    // int price = 100 ;
+    // cout << "The price is : " <<  price << "$";
+    // cout << "\nThe price after adding 15$ : " <<  price + 15 << "$" ;
+    // cout << "\nThe price after adding 50$ : " <<  price + 50 << "$";
+    // cout << "\nTotal : " << price + 15 + 50 << "$";
+    // price = 50 ; 
+    // cout << "\nThe new price is  : " << price  << "$";
+    
+
+    return 0; 
+}
