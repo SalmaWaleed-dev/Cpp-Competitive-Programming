f0e28c7271faaa2e44443c0fc7aa2f77e7aa9bb8
salma waleed <salmawaleed20067@gmail.com>
2026-09-25 08:22:31 +0300

    studing some topics

--- c++ fundamentals/app.cpp
+++ c++ fundamentals/app.cpp
@@ -12,6 +12,32 @@ int main()
     // price = 50 ; 
     // cout << "\nThe new price is  : " << price  << "$";
     
+    // ! we have a company and we want to pt the values of the working hours  = 8 hr and mohammed has salary = 15000 and we can change it in another time into a 20000 .
+    // const int workingHours = 8 ;
+    // int sallary = 15000; 
+    // cout << "Working hours in the comnpany : " << workingHours << "hr" << "\nMohammed's Sallary : " << sallary << "$" ;
+    // sallary = 20000 ;
+    // cout << "\nMohammed's New sallary : " << sallary << "$" ;
+
+    // ! Calculate your age Application 
+    // cout << "***********************************\n";
+    // cout << "** Calculate your age Application **\n";
+    // cout << "***********************************\n";
+
+    //     int birthYear;
+    //     int currentYear;
+
+    // cout << "Enter your Birth Year : " ;
+    // cin>>birthYear ;
+    // cout << "Enter The Current Year : " ;
+    // cin>>currentYear;
+    // int age = currentYear - birthYear ; 
+    // int ageInDays = age * 365.25 ;
+    // int ageInHours = ageInDays * 24 ;
+    // cout << "Your Age is : " << age << " Years \n" ;
+    // cout << "Age In Days Is : " << ageInDays << " Days\n";
+    // cout << "Age In Houes Is : " << ageInHours << " Hours\n";
+
 
     return 0; 
 }
